97b16a4051ed221ace4dd4be21f5163491b3b0f0
Ivan Sokolovskii <ivan3177@gmail.com>
2020-04-01 00:25:58 +0300

    Initial Commit

---
+++ Hiking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by Иван Соколовский on 01.04.2020.
+//  Copyright © 2020 Ivan Sokolovskii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
